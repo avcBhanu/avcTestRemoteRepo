e1053b839f8b2d254a4462cd9eec3a6d1ef46e8e
Bhanu <avc.bhanu@gmail.com>
2019-08-28 01:15:20 +0530

    added new project

---
+++ AvcFirstJava/AVCArtifact/src/main/java/AVCGroup/AVCArtifactPackage/App.java
@@ -0,0 +1,13 @@
+package AVCGroup.AVCArtifactPackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AvcFirstJava/AVCArtifact/src/test/java/AVCGroup/AVCArtifactPackage/AppTest.java
@@ -0,0 +1,20 @@
+package AVCGroup.AVCArtifactPackage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
